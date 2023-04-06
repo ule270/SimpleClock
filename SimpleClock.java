9c1e37e3f430a25b7a8ad5a5201ffba7644b40be
ule270 <u.le270@gmail.com>
2023-04-06 15:53:52 -0400

    military time button added

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,6 +2,7 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -12,7 +13,10 @@ public class SimpleClock extends JFrame {
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
-    
+
+        JButton milTime;
+        JButton localGMT;
+        boolean isMilitary;
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
@@ -25,32 +29,45 @@ public class SimpleClock extends JFrame {
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
             this.setSize(350, 220);
-            this.setResizable(false);
+            this.setResizable(false); //cannot drag the corner to change the size;
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
-            dayFormat=new SimpleDateFormat("EEEE");
-            dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
+            dayFormat= new SimpleDateFormat("EEEE");
+            dateFormat= new SimpleDateFormat("dd MMMMM, yyyy");
             timeLabel = new JLabel();
             timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
             timeLabel.setBackground(Color.BLACK);
             timeLabel.setForeground(Color.WHITE);
             timeLabel.setOpaque(true);
-            dayLabel=new JLabel();
+            dayLabel= new JLabel();
             dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
+
+            milTime = new JButton ("Military");
+            milTime.addActionListener(this::militaryStandard);
     
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
-            this.setVisible(true);
+            this.add(milTime);
+            this.setVisible(true); // to be able to be visible;
     
             setTimer();
         }
-    
-        public void setTimer() {
+
+    private void militaryStandard(ActionEvent actionEvent) {
+            if (isMilitary){
+            timeFormat = new SimpleDateFormat("hh:mm:ss a");
+            isMilitary = false;
+        } else {
+            timeFormat = new SimpleDateFormat("HH:mm:ss a");
+            isMilitary = true;
+        }
+    }
+
+    public void setTimer() {
             while (true) {
                 time = timeFormat.format(Calendar.getInstance().getTime());
                 timeLabel.setText(time);
@@ -69,6 +86,7 @@ public class SimpleClock extends JFrame {
             }
         }
         public static void main(String[] args) {
+
             new SimpleClock();
         }
     }
